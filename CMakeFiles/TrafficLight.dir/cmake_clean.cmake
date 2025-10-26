ec5ce3639b1007432e6f1ae6f497d8d9db4c20e7
ssk <3911261819@qq.com>
2025-10-26 17:04:56 +0800

    添加TrafficLight项目

---
+++ CMakeFiles/TrafficLight.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TrafficLight.dir/traffic/test.cpp.o"
+  "CMakeFiles/TrafficLight.dir/traffic/test.cpp.o.d"
+  "TrafficLight"
+  "TrafficLight.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TrafficLight.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
